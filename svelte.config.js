d00ebb04ba5a695d710bd93c60fb750198ca3253
zeyus <zeyus@zeyus.com>
2025-02-14 13:48:40 +0100

    added gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,23 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: ''
+		},
+		prerender: {
+			handleHttpError: 'warn',
+			crawl: true,
+			handleMissingId: 'warn',
+
+		},
 	}
 };
 
